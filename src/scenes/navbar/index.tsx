c6ac33e9e2eb16e87c7d56a14a73db74a6132d94
Mrityunjay-Palled <113463073+Mrityunjay-Palled@users.noreply.github.com>
2023-03-08 21:51:28 +0530

    added action button

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -47,7 +47,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
-                  <button>Become a Member</button>
+                  {/* <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton> */}
                 </div>
               </div>
             ) : (
