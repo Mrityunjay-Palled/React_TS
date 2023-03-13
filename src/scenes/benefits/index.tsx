daad7d44696e06d96aadc1873f6c0bb9b45975e4
Mrityunjay-Palled <113463073+Mrityunjay-Palled@users.noreply.github.com>
2023-03-13 23:27:49 +0530

    added stagger animation for benefit cards

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -21,6 +21,15 @@ const benefits:Array<BenefitType>=[
     description:"lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet lorem ipsum dolor sit amet"
   },
 ]
+ 
+
+const container={
+  hidden:{},
+  visible:{
+    transition:{staggerChildren:0.2}
+  },
+}
+
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
   };
@@ -35,13 +44,28 @@ const Benefits=({ setSelectedPage }: Props)=> {
     <motion.div
     onViewportEnter={()=>setSelectedPage(SelectedPage.Home)}
     >
-     <div className="md:my-5 md:w-3/5">
+     <motion.div className="md:my-5 md:w-3/5"
+     initial="hidden"
+     whileInView="visible"
+     viewport={{ once: true, amount: 0.5 }}
+     transition={{ delay: 0.2, duration: 0.5 }}
+     variants={{
+       hidden: { opacity: 0, x: -50 },
+       visible: { opacity: 1, x: 0 },
+     }}
+     >
         <HText>MORE THAN JUST GYM</HText>
         <p className="my-5 text-sm">
           We provide world class fitness equipment,trainers and classes to get you to your ultimate fitness goals with ease. We provide true care into each and every member
         </p>
-     </div>
-     <div className="md:flex items-center justify-between gap-8 mt-5">
+     </motion.div>
+     <motion.div 
+     className="md:flex items-center justify-between gap-8 mt-5"
+     initial="hidden"
+     whileInView="visible"
+     viewport={{once:true,amount:0.5}}
+     variants={container}
+     >
        {benefits.map((benefit:BenefitType)=>(
         <Benefit
          key={benefit.title}
@@ -51,7 +75,7 @@ const Benefits=({ setSelectedPage }: Props)=> {
          setSelectedPage={setSelectedPage}
         />
        ))}
-     </div>
+     </motion.div>
     </motion.div>
     </section>
   )
